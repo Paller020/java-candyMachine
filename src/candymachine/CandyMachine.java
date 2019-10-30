842fb604b1d5a98aaa9b38a46d6fc7e85f36950b
21016 <21016@MININT-RTINT45.columbushs.com>
2019-10-30 09:35:21 -0400

    In Progress

--- src/candymachine/CandyMachine.java
+++ src/candymachine/CandyMachine.java
@@ -4,24 +4,82 @@
  * and open the template in the editor.
  */
 package candymachine;
+import java.util.Scanner;
 
-/**
- *
- * @author mlarrubia
- */
+
 public class CandyMachine {
 
-    /**
-     * @param args the command line arguments
-     */
+   private static Scanner kb = new Scanner(System.in);
+    
+    
+    
     public static void main(String[] args) {
+        
+        
         System.out.println("Welcome to CHS's Comptuer Candy Machine!");
         System.out.println("All candy provided is virtual.");
                 
-        // Happy Coding!
+       System.out.print("How much money do ya got? > ");
+       double money1 = kb.nextDouble(); 
+       System.out.println("Well, lemme tell ya what we got here. ");
+       System.out.println("A          $1.00    Twix");
+       System.out.println("B          $0.25    Gum");
+       System.out.println("C          $0.75    Chips");
+       System.out.println("D          $1.00    Skittles");
+       System.out.println("E          $0.50    Gummy Bears");
+       candy();
+    }
+    public static double candy()
+    {
+     System.out.print("So, What'll ya have? > ");
+     String choice = kb.nextLine();
+     if(choice.equalsIgnoreCase("A"))
+     {
+         return 1.00;
+     }    
+     else if(choice.equalsIgnoreCase("B")) 
+     {
+         return 0.25;
+     }    
+     else if(choice.equalsIgnoreCase("C"))
+     {
+         return 0.75;
+     }  
+     else if(choice.equalsIgnoreCase("D"))
+     {
+         return 1.00; 
+     }    
+     else if(choice.equalsIgnoreCase("E"))
+     { 
+         return 0.50;
+     }  
+     else
+     {
+         return 0;
+     }    
+        
+    
+        
+        
+        
+        
+        
+        
+        
         
         
         
     }
+      
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
 }
